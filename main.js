38381bafb06cd9acdce2d694d4c9fbfc178151b7
CandelariaCabrera <canddi1d23@gmail.com>
2023-11-01 19:44:03 -0600

    menu mobile

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ var typed = new Typed (".text",{
     backDelay:1000,
     loop: true
 });
+
+
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
